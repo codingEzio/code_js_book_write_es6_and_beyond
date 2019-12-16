13a5c8c2d70bcfdde87498f86e7661e9287af674
Ezio <assassinste@gmail.com>
2019-12-16 20:29:57 +0800

    A word about keyword 'delete' (Node.js)

---
+++ trivial_stuff_you_dont_need_to_know/keyword_delete.js
@@ -0,0 +1,9 @@
+// As you're learning JavaScript or node.js, you probably have typed
+// some code in the node.js REPL, which is a pretty great REPL I think.
+
+// Sometimes you might wanna reset a variable, like `delete` in Python,
+// just so you know, any vars being defined by 'var', 'let' cannot be
+// deleted.
+
+// Why?
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete
